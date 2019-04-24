e3fa7f1fa1066ae69c72783b0d755828b586cbed
skaymen <stanley.kaymen@gmail.com>
2019-04-24 16:59:08 -0500

    alter messages page, add table and boxes

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Button } from "react-bootstrap";
+import ReactTable from "react-table";
 
 import "../styles/Messages.css";
 
@@ -7,6 +8,19 @@ import Play from "../images/play.svg";
 import Forward from "../images/skipforward.svg";
 import Back from "../images/skipback.svg";
 import Refresh from "../images/update-arrows.svg";
+
+import messagedata from "../data/messages.json";
+
+const columns = [
+  {
+    Header: "Message Text",
+    accessor: "message_text" // String-based value accessors!
+  },
+  {
+    Header: "Decoded Data",
+    accessor: "decoded_data"
+  }
+];
 
 //messaging component for the database editor
 class Messages extends Component {
@@ -33,7 +47,9 @@ class Messages extends Component {
           "Maecenas sed enim ut sem viverra aliquet. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Faucibus purus in massa tempor nec feugiat nisl. Enim ut tellus elementum sagittis vitae. Adipiscing commodo elit at imperdiet dui accumsan sit amet. Urna molestie at elementum eu facilisis sed odio morbi quis. Morbi tristique senectus et netus et. A iaculis at erat pellentesque adipiscing commodo elit at imperdiet. Neque volutpat ac tincidunt vitae semper quis lectus nulla. Nulla posuere sollicitudin aliquam ultrices sagittis. Quam pellentesque nec nam aliquam sem. Massa enim nec dui nunc mattis. Quis hendrerit dolor magna eget est lorem. Rutrum quisque non tellus orci ac auctor augue mauris augue. Placerat orci nulla pellentesque dignissim enim sit amet venenatis. Eget mi proin sed libero enim sed. Ut porttitor leo a diam. Turpis egestas integer eget aliquet nibh praesent. Ullamcorper malesuada proin libero nunc consequat interdum varius. Adipiscing enim eu turpis egestas pretium aenean.",
           "Elit eget gravida cum sociis natoque penatibus. Nibh tortor id aliquet lectus proin nibh. Sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Blandit turpis cursus in hac habitasse. Nibh mauris cursus mattis molestie a. In dictum non consectetur a erat nam. Justo nec ultrices dui sapien eget mi. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat. Viverra nibh cras pulvinar mattis nunc. At erat pellentesque adipiscing commodo elit at. Adipiscing tristique risus nec feugiat in fermentum. A arcu cursus vitae congue mauris. Amet massa vitae tortor condimentum lacinia quis. Nulla aliquet porttitor lacus luctus."
         ]
-      }
+      },
+      selected: null
+
     };
   }
 
@@ -66,43 +82,92 @@ class Messages extends Component {
   }
 
   render() {
+    let data = messagedata;
     return (
       <div id="messages">
-      {/* list relevant informaiton along the top */}
+        {/* list relevant informaiton along the top */}
         <div id="top-boxes">
           <div className="text-box">
             <h3>Message Source</h3>
-            <p>{this.state.demo_data.source}</p>
+            {/* <p>{this.state.demo_data.source}</p> */}
+            <input/>
           </div>
           <div className="text-box">
             <h3>Location / Transmission ID</h3>
-            <p>{this.state.demo_data.location}</p>
+            {/* <p>{this.state.demo_data.location}</p> */}
+            <input/>
           </div>
           <div className="text-box">
             <h3>Time Period</h3>
-            <p>{this.state.demo_data.time_period}</p>
+            {/* <p>{this.state.demo_data.time_period}</p> */}
+            <input/>
           </div>
           <div className="text-box">
             <h3>Message Output Format</h3>
-            <p>{this.state.demo_data.output_format}</p>
+            {/* <p>{this.state.demo_data.output_format}</p> */}
+            <input/>
           </div>
         </div>
         <div id="message-data-box">
           <div id="message-data">
-            <h3>
+          <ReactTable
+          data={data}
+          columns={columns}
+          // set the default number of rows to display. take away ability to change thos.
+          defaultPageSize={5}
+          showPageSizeOptions={false}
+          //this is code to make elements within the table selectable
+          getTrProps={(state, rowInfo) => {
+            if (rowInfo && rowInfo.row) {
+              return {
+                onClick: (e) => {
+                  this.setState({
+                    selected: rowInfo.index
+                  })
+                },
+                style: {
+                  background: rowInfo.index === this.state.selected ? '#00afec' : 'white',
+                  color: rowInfo.index === this.state.selected ? 'white' : 'black'
+                }
+              }
+            }else{
+              return {}
+            }
+          }}
+         />
+            {/* <h3>
               {/* print message data, page number, etc */}
-              Message Data - {this.state.index + 1} of{" "}
+              {/* Message Data - {this.state.index + 1} of{" "}
               {this.state.demo_data.message_text.length}
               {/* <button id="refresh-button" onClick={this.refresh}>
                 <img src={Refresh} alt="refresh" id="refresh"/>
               </button> */}
-              <input type="image" src={Refresh} id="refresh" alt="refresh" onClick={this.refresh}/>
-            </h3>
-            <p>{this.state.demo_data.message_text[this.state.index]}</p>
+              {/* <input
+                type="image"
+                src={Refresh}
+                id="refresh"
+                alt="refresh"
+                onClick={this.refresh}
+              /> */}
+            {/* </h3> */}
+            {/* <Dropdown>
+              <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                Dropdown Button
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown> */}
+            {/* <div id="data-display">
+              <p>{this.state.demo_data.message_text[this.state.index]}</p>
+            </div> */}
           </div>
 
           {/* buttons for skip and play */}
-          <div id="buttons">
+          {/* <div id="buttons">
             <Button
               variant="outline-info"
               className="message-button"
@@ -129,7 +194,7 @@ class Messages extends Component {
                 id="forward"
               />
             </Button>
-          </div>
+          </div> */}
         </div>
       </div>
     );
